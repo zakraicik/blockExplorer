69b313cbbfbd6c0f85e02332c085e52ab080e122
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-09 06:30:22 -0700

    Centered blocks

--- src/components/ScrollableChain.jsx
+++ src/components/ScrollableChain.jsx
@@ -9,28 +9,27 @@ const data = [
 ];
 
 const ScrollableChain = () => {
-  const [currentIndex, setCurrentIndex] = useState(data.length - 1); // Start from the last item
+  const [currentIndex, setCurrentIndex] = useState(0); 
   const scrollRef = useRef(null);
 
   const nextSlide = () => {
-    if (currentIndex < data.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-      scrollRef.current.scrollLeft += scrollRef.current.offsetWidth / 3;
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      scrollRef.current.scrollLeft += scrollRef.current.scrollWidth / 3;
     }
   };
 
   const prevSlide = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-      scrollRef.current.scrollLeft -= scrollRef.current.offsetWidth / 3;
+    if (currentIndex < data.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      scrollRef.current.scrollLeft -= scrollRef.current.scrollWidth / 3;
     }
   };
 
   useEffect(() => {
     const adjustScrollPosition = () => {
-      const containerWidth = scrollRef.current.offsetWidth;
       const totalScrollWidth = scrollRef.current.scrollWidth;
-      scrollRef.current.scrollLeft = totalScrollWidth - containerWidth;
+      scrollRef.current.scrollLeft = totalScrollWidth 
     };
 
     adjustScrollPosition();
@@ -42,19 +41,19 @@ const ScrollableChain = () => {
 
   return (
     <div>
-      <h3>Current Index: {currentIndex}</h3>
-      <h3>Current Title: {data[currentIndex].title}</h3>
       <div className="scrollable-chain-wrapper">
         <button onClick={prevSlide} className="chain-button left">
           &#10094;
         </button>
         <div className="scrollable-chain-container">
           <div className="scrollable-chain" ref={scrollRef}>
+            <div className="chain-item invisible"></div> {/* Invisible padding block */}
             {data.map((item) => (
               <div key={item.id} className="chain-item">
                 <h3>{item.title}</h3>
               </div>
             ))}
+            <div className="chain-item invisible"></div> {/* Invisible padding block */}
           </div>
         </div>
         <button onClick={nextSlide} className="chain-button right">
