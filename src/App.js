b436be003c31d1d2d779f8e0e63e900b1766e8d7
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-09 20:51:44 -0700

    Started cleaning up CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Alchemy, Network, Utils } from 'alchemy-sdk'
+import { Alchemy, Network } from 'alchemy-sdk'
 import { useEffect, useState, useCallback } from 'react'
 import ScrollableChain from './components/ScrollableChain'
-import {
-  getBlockDetails,
-  formatTimestamp,
-  truncateAddress
-} from './helper/alchemyHelpers'
+import BlockSummary from './components/BlockSummary'
+
+import { getBlockDetails } from './helper/alchemyHelpers'
 
 import './App.css'
 
@@ -20,7 +18,7 @@ function App () {
   const [blockNumbers, setBlockNumbers] = useState([])
   const [selectedBlock, setSelectedBlock] = useState(null)
   const [blockDetails, setBlockDetails] = useState(null)
-  const [blockTransactionDetails, setBlockTransactionDetails] = useState(null)
+  // const [blockTransactionDetails, setBlockTransactionDetails] = useState(null)
 
   // Block numbers
   useEffect(() => {
@@ -52,82 +50,25 @@ function App () {
 
   return (
     <div className='App'>
-      <div>
+      <div className='scrollable-chain-parent-container'>
         <ScrollableChain
           blockNumbers={blockNumbers}
           setSelectedBlock={setSelectedBlock}
-          selectedBlock={selectedBlock} // Pass selectedBlock as a prop
+          selectedBlock={selectedBlock}
         />
-        <div className='row-container'>
-          <div className='information-parent-container'>
-            <div className='information-container'>
-              {selectedBlock ? (
-                <>
-                  <div className='block-number'>
-                    Block #{selectedBlock.blockNumber}
-                  </div>
-                  {blockDetails ? (
-                    <>
-                      <p className='block-timestamp'>
-                        {formatTimestamp(blockDetails.timestamp)}
-                      </p>
-
-                      <p className='block-details'>
-                        <span className='block-label'>Mined By:</span>
-                        <span className='block-value'>
-                          {truncateAddress(blockDetails.miner)}
-                        </span>
-                      </p>
-
-                      <p className='block-details'>
-                        <span className='block-label'>
-                          Number Transactions:
-                        </span>
-                        <span className='block-value'>
-                          {blockDetails.transactions.length.toLocaleString()}
-                        </span>
-                      </p>
-
-                      <p className='block-details'>
-                        <span className='block-label'>Gas Limit:</span>
-                        <span className='block-value'>
-                          {Number(blockDetails.gasLimit._hex).toLocaleString()}
-                        </span>
-                      </p>
-                      <p className='block-details'>
-                        <span className='block-label'>Gas Used:</span>
-                        <span className='block-value'>
-                          {Number(blockDetails.gasUsed._hex).toLocaleString()}
-                        </span>
-                      </p>
-
-                      <p className='block-details'>
-                        <span className='block-label'>Base Fee Per Gas:</span>
-                        <span className='block-value'>
-                          {Utils.formatUnits(
-                            Number(blockDetails.baseFeePerGas._hex),
-                            'ether'
-                          )}
-                        </span>
-                      </p>
-
-                      <p className='block-details'>
-                        <span className='block-label'>Nonce: </span>
-                        <span className='block-value'>
-                          {Number(blockDetails.nonce)}
-                        </span>
-                      </p>
-                    </>
-                  ) : (
-                    <p>Loading block details...</p>
-                  )}
-                </>
-              ) : (
-                <p>Select a block to see more information</p>
-              )}
-            </div>
-          </div>
-          <div className='extra-container'></div>
+      </div>
+      <div className='row-container'>
+        <div className='information-parent-container'>
+          <BlockSummary
+            selectedBlock={selectedBlock}
+            blockDetails={blockDetails}
+          />
+        </div>
+        <div className='information-parent-container'>
+          <BlockSummary
+            selectedBlock={selectedBlock}
+            blockDetails={blockDetails}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/BlockSummary.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Utils } from 'alchemy-sdk'
+import { formatTimestamp, truncateAddress } from '../helper/alchemyHelpers'
+
+import '../App.css'
+
+const BlockSummary = ({ selectedBlock, blockDetails }) => {
+  return (
+    <div className='information-container'>
+      {selectedBlock ? (
+        <>
+          <div className='block-number'>Block #{selectedBlock.blockNumber}</div>
+          {blockDetails ? (
+            <>
+              <p className='block-timestamp'>
+                {formatTimestamp(blockDetails.timestamp)}
+              </p>
+
+              <p className='block-details'>
+                <span className='block-label'>Mined By:</span>
+                <span className='block-value'>
+                  {truncateAddress(blockDetails.miner)}
+                </span>
+              </p>
+
+              <p className='block-details'>
+                <span className='block-label'>Number Transactions:</span>
+                <span className='block-value'>
+                  {blockDetails.transactions.length.toLocaleString()}
+                </span>
+              </p>
+
+              <p className='block-details'>
+                <span className='block-label'>Gas Limit:</span>
+                <span className='block-value'>
+                  {Number(blockDetails.gasLimit._hex).toLocaleString()}
+                </span>
+              </p>
+              <p className='block-details'>
+                <span className='block-label'>Gas Used:</span>
+                <span className='block-value'>
+                  {Number(blockDetails.gasUsed._hex).toLocaleString()}
+                </span>
+              </p>
+
+              <p className='block-details'>
+                <span className='block-label'>Base Fee Per Gas:</span>
+                <span className='block-value'>
+                  {Utils.formatUnits(
+                    Number(blockDetails.baseFeePerGas._hex),
+                    'ether'
+                  )}
+                </span>
+              </p>
+
+              <p className='block-details'>
+                <span className='block-label'>Nonce: </span>
+                <span className='block-value'>
+                  {Number(blockDetails.nonce)}
+                </span>
+              </p>
+            </>
+          ) : (
+            <p>Loading block details...</p>
+          )}
+        </>
+      ) : (
+        <p>Select a block to see more information</p>
+      )}
+    </div>
+  )
+}
+
+export default BlockSummary
